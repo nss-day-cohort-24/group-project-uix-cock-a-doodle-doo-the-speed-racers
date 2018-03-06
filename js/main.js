3bf742675b241864c3a31c6a5ffd66881262844d
Sam Kimball <jibequan@gmail.com>
2018-03-06 16:05:47 -0600

    Added XHR to access data

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
 "use strict";
 
-console.log("Hello main.js");
+// console.log("Hello main.js");
+
+let news = require('./news_api');
+
+// news.api_calls.getHeadlines();
+news.api_calls.giveHeadlines();
