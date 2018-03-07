d312abf517a35bb1f0846845735bcdb427474eda
ROHINI CHAVAN <rohinichavan@ROHINIs-MacBook-Pro.local>
2018-03-07 14:47:54 -0600

    meetup-api function

--- js/meetup-api.js
+++ js/meetup-api.js
@@ -1,30 +1,6 @@
 "use strict";
 let $ = require('jquery');
-//let printMeetup = require('./dom-builder');
-
-
-//function to print the list of meetups
-function meetupToRenderToDom(meetupList) {
-    console.log("meetupList", meetupList);
-  if(meetupList.events){
-  for (let i = 0; i < 10; i++) {
-      let eventsArray = meetupList.events;
-      console.log("eventsArray", eventsArray);
-          console.log("group name:", eventsArray[i].group.name);
-          console.log("date:",eventsArray[i].local_date);
-          console.log("time:",eventsArray[i].local_time);
-          console.log("venue name:", eventsArray[i].venue.name);
-          console.log("venue address:", eventsArray[i].venue.address_1); 
-          console.log("meetup link", eventsArray[i].link);
-          $('#meetups').append(`<li class="meetupevent">
-          <h2>${eventsArray[i].group.name}</h2>
-          <h3>${eventsArray[i].local_date} ${eventsArray[i].local_time}</h3>
-          <p>${eventsArray[i].venue.name} - ${eventsArray[i].venue.address_1}<br>
-          <a target="_blank" href="${eventsArray[i].link}">learn more</a></li>`);       
-      } 
-    }
-}
-
+let printMeetup = require('./dom-builder');
 
 //function to call the meetup APIs
 function callMeetupApi(url) {
@@ -43,7 +19,7 @@ function callMeetupApi(url) {
         // console.log("meetupList", meetupList);
          let eventsArray = meetupData.events;
         console.log("eventsArray", eventsArray);
-        meetupToRenderToDom(meetupData);
+        printMeetup.meetupToRenderToDom(meetupData);
     },
     (reject) => {
     console.log("SOMETHING WENT REALLY WRONG");
