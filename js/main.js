0594c7e64829766c05e22e60ef5279c613f7af99
Sam Kimball <jibequan@gmail.com>
2018-03-08 09:53:16 -0600

    Required user.js and added login/signout functionality

--- js/main.js
+++ js/main.js
@@ -7,6 +7,7 @@ let news_api = require('./news_api');
 let newsToDom = require('./dom-builder');
 require("../js/weather.js");
 let books = require('./books_api');
+let login = require("./user");
 
 
 meetupAPI.callMeetupApi("https://api.meetup.com/find/upcoming_events?photo-host=public&topic_category=technology&page=10&sig_id=194426544&lon=-86.7816&lat=36.1627&sig=fc94244c44f01706117c5e4edfc04d3b9279afdd&key=4d1a4e4c161d807435128114c225176")
@@ -22,4 +23,21 @@ meetupAPI.callMeetupApi("https://api.meetup.com/find/upcoming_events?photo-host=
 news_api.getHeadlines()
   .then((resolve) => {
     newsToDom.publishNews(resolve);
+});
+
+$("#auth-btn").click(function(){
+  console.log("clicked on Signin");
+  login.logInGoogle()
+  .then((result) => {
+    console.log("result from login", result.user.uid);
+    login.setUser(result.user.uid);
+    $("#auth-btn").addClass("is-hidden");
+    $("#logout").removeClass("is-hidden");
+    // loadSongsToDOM();
+  });
+});
+
+$("#logout").click(function(){
+  console.log("logout clicked");
+
 });
