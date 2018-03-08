67b5f0a083f398be88d42c562bc1832f23003f77
Sam Kimball <jibequan@gmail.com>
2018-03-08 10:51:22 -0600

    Moved print to DOM methods
    Moved them inside auth block to prevent visiblity until user is logged in.

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 "use strict";
 
 console.log("Hello main.js");
+let $ = require('jquery');
 let meetupAPI = require('./meetup-api.js');
 let printMeetup = require('./dom-builder');
 let news_api = require('./news_api');
@@ -10,21 +11,6 @@ let books = require('./books_api');
 let login = require("./user");
 
 
-meetupAPI.callMeetupApi("https://api.meetup.com/find/upcoming_events?photo-host=public&topic_category=technology&page=10&sig_id=194426544&lon=-86.7816&lat=36.1627&sig=fc94244c44f01706117c5e4edfc04d3b9279afdd&key=4d1a4e4c161d807435128114c225176")
-    .then((resolve) => {
-        console.log("meetupArray", resolve);
-        printMeetup.meetupToRenderToDom(resolve);
-    },
-    (reject) => {
-    console.log("SOMETHING WENT REALLY WRONG");
-    });
-
-
-news_api.getHeadlines()
-  .then((resolve) => {
-    newsToDom.publishNews(resolve);
-});
-
 $("#auth-btn").click(function(){
   console.log("clicked on Signin");
   login.logInGoogle()
@@ -33,7 +19,18 @@ $("#auth-btn").click(function(){
     login.setUser(result.user.uid);
     $("#auth-btn").addClass("is-hidden");
     $("#logout").removeClass("is-hidden");
-    // loadSongsToDOM();
+    news_api.getHeadlines()
+      .then((resolve) => {
+        newsToDom.publishNews(resolve);
+    });
+    meetupAPI.callMeetupApi("https://api.meetup.com/find/upcoming_events?photo-host=public&topic_category=technology&page=10&sig_id=194426544&lon=-86.7816&lat=36.1627&sig=fc94244c44f01706117c5e4edfc04d3b9279afdd&key=4d1a4e4c161d807435128114c225176")
+        .then((resolve) => {
+            console.log("meetupArray", resolve);
+            printMeetup.meetupToRenderToDom(resolve);
+        },
+        (reject) => {
+        console.log("SOMETHING WENT REALLY WRONG");
+        });
   });
 });
 
