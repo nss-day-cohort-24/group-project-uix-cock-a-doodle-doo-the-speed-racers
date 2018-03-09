8d3c72f1372b030347d15602e632ea55d68b3ab1
Sam Kimball <jibequan@gmail.com>
2018-03-09 13:14:24 -0600

    Create function to prep article for saving to db

--- js/db-interaction.js
+++ js/db-interaction.js
@@ -3,7 +3,17 @@
 console.log("Hello db-interaction.js");
 
 let firebase = require("./fb-config");
+let headlines = require("./dom-builder");
 
+function prepArticleForSaving(saveButton) {
+  let particularArticle = saveButton.parentNode.id;
+  let idPieces = particularArticle.split("--");
+  let position = idPieces[1];
+  let headlineList = headlines.giveAllHeadlines();
+  console.log(headlineList);
+  let targetArticle = headlineList.{position};
+  console.log(targetArticle);
+  return targetArticle;
+}
 
-
-module.exports = {};
+module.exports ={prepArticleForSaving};
